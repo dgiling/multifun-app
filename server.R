06337502b588e1cb52e07d96d0cf90260721a05a
Darren Giling <dgiling@gmail.com>
2018-08-27 15:27:18 +0200

    initial commit of multifun-app
    This application is designed to assist researchers investigating the effects of global change on ecosystem multifunctionality (i.e. GC-EMF research) to develop mechanistic hypotheses.

---
+++ server.R
@@ -0,0 +1,269 @@
+
+library(shiny)
+
+# Functions
+plot.simple <- function(data, x, y, xlab, ylab) {
+  ggplot(data, aes_string(x=x, y=y)) +
+    geom_point(size=3, alpha=0.2) +
+    #labs(title=main) +
+    scale_y_continuous(name=ylab) +
+    scale_x_continuous(name=xlab) +
+    theme_bw(base_size=15) +
+    #stat_smooth(method="lm", colour="black", size=1, se=F) +
+    theme(panel.grid = element_blank())
+}
+
+cor.mtest <- function(mat, ...) {
+  mat <- as.matrix(mat)
+  n <- ncol(mat)
+  p.mat<- matrix(NA, n, n)
+  diag(p.mat) <- 0
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      tmp <- cor.test(mat[, i], mat[, j], ...)
+      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+    }
+  }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+  p.mat
+}
+
+
+# Define server logic
+shinyServer(function(input, output, session) {
+  
+  observeEvent(input$gen.data, {
+    updateTabsetPanel(session, "main",
+                      selected = "Data simulation results")
+  })
+  
+  ## Generate data table
+  dt <- reactive ({   
+
+    input$gen.data
+    
+    # Define GCD
+    isolate({
+      def <- defData(varname = "GCD", dist = "uniformInt", formula = paste0("1;",input$nlev))
+    })
+    
+    # Step 1
+    if(input$dir == "left") { 
+      isolate({
+      e1 <- renderText({ input$eqn1.l }, quoted=T)
+      e1var <- input$eqn1var.l
+      e2 <- renderText({ input$eqn2.l }, quoted=T)
+      e2var <- input$eqn2var.l
+      })
+      def <- defData(def, dist="normal", varname = "M2", formula = e2(), variance = e2var) 
+      def <- defData(def, dist="normal", varname = "M1", formula = e1(), variance = e1var)
+    }
+    if(input$dir == "right") { 
+      isolate({
+      e1 <- renderText({ input$eqn1.r }, quoted=T)
+      e1var <- input$eqn1var.r
+      e2 <- renderText({ input$eqn2.r }, quoted=T)
+      e2var <- input$eqn2var.r
+      })
+      def <- defData(def, dist="normal", varname = "M1", formula = e1(), variance = e1var)
+      def <- defData(def, dist="normal", varname = "M2", formula = e2(), variance = e2var) 
+    }
+  
+    # Step 2
+    isolate({
+    e3 <- renderText({ input$eqn3 }, quoted=T)
+    e3var <- input$eqn3var
+    e4 <- renderText({ input$eqn4 }, quoted=T)
+    e4var <- input$eqn4var
+    e5 <- renderText({ input$eqn5 }, quoted=T)
+    e5var <- input$eqn5var    
+    e6 <- renderText({ input$eqn6 }, quoted=T)
+    e6var <- input$eqn6var
+    })
+    
+    def <- defData(def, dist="normal", varname = "F1", formula = e3(), variance = e3var)
+    def <- defData(def, dist="normal", varname = "F2", formula = e4(), variance = e4var) 
+    def <- defData(def, dist="normal", varname = "F3", formula = e5(), variance = e5var)
+    def <- defData(def, dist="normal", varname = "F4", formula = e6(), variance = e6var) 
+    
+    # Generate data
+    isolate({ sim.data <- genData(input$nobs, def) })
+
+    # calculate EMF
+    # 1. averaging
+    vars <- c("F1", "F2", "F3", "F4")
+    sim.data <- cbind(sim.data, getStdAndMeanFunctions(sim.data, vars)) # standardises between 0 and 1, takes average
+    
+    # 2. threshold
+    vars.std <- c("F1.std", "F2.std", "F3.std", "F4.std")
+    thresh <- getFuncsMaxed(sim.data, vars.std, threshmin=0.05, threshmax=0.99, prepend=c("id","GCD"))
+
+    thresh25 <- thresh[thresh$thresholds=="0.25",c("id","GCD","funcMaxed")]
+    names(thresh25)[3] <- "funcMaxed25"
+    thresh50 <- thresh[thresh$thresholds==0.50,c("id","GCD","funcMaxed")]
+    names(thresh50)[3] <- "funcMaxed50"
+    thresh75 <- thresh[thresh$thresholds=="0.75",c("id","GCD","funcMaxed")]
+    names(thresh75)[3] <- "funcMaxed75"
+    
+    sim.data <- merge(sim.data, thresh25, by=c("id","GCD"))
+    sim.data <- merge(sim.data, thresh50, by=c("id","GCD"))
+    sim.data <- merge(sim.data, thresh75, by=c("id","GCD"))
+    
+    sim.data
+
+  })
+    
+    
+  ## Generate plots
+  # Output plot A: Step 1
+  output$plotA <- renderPlot({
+    
+      plot1 <- plot.simple(data=dt(), x="GCD", y="M2", xlab="Global change driver", ylab="Mechanism 1 (M1)") #, main = "Equation 1")
+      
+      if(input$dir == "left") {  
+       plot2 <- plot.simple(data=dt(), x="M2", y="M1", xlab="Mechanism 2 (M2)", ylab="Mechanism 1 (M1)") #, main = "Equation 1")
+      }
+      if(input$dir == "right") {  
+       plot2 <- plot.simple(data=dt(), x="M1", y="M2", xlab="Mechanism 1 (M1)", ylab="Mechanism 2 (M2)") #, main = "Equation 2")
+      }
+       
+      plot3 <- plot.simple(data=dt(), x="GCD", y="M2", xlab="Global change driver", ylab="Mechanism 2 (M2)") #, main = "Equation 2")
+      plot_grid(plot1, plot2, plot3, ncol=3, align = 'h')
+
+  })
+  
+  # Output plot B: Step 2, biotic effects
+  output$plotB <- renderPlot({
+  
+      #vars.std <- c("F1.std", "F2.std", "F3.std", "F4.std")
+      vars <- c("F1", "F2", "F3", "F4")
+      
+      f.long <- melt(dt(), id.vars=c("GCD","M2","M1"), measure.vars = vars)
+      levels(f.long$variable) <- c("F1", "F2", "F3", "F4")
+      
+      ggplot(f.long, aes(x=M1, y=value)) +
+        facet_wrap(~variable, ncol=4) +
+        #geom_point(size=3, color = "#049C8E", alpha=0.3) + # biotic green
+        geom_point(size=3, alpha=0.3) +
+        scale_y_continuous(name="Function") +
+        scale_x_continuous(name="Mechanism 1 (M1)") +
+        theme_bw(base_size=15) +
+        #stat_smooth(method="lm", colour="black", size=1, se=T) +
+        theme(panel.grid = element_blank(), strip.background = element_blank())
+      
+  })
+  
+  # Output plot C: Step 2, abiotic effects
+  output$plotC <- renderPlot({
+    
+    vars.std <- c("F1.std", "F2.std", "F3.std", "F4.std")
+    vars <- c("F1", "F2", "F3", "F4")
+    
+    f.long <- melt(dt(), id.vars=c("GCD","M2","M1"), measure.vars = vars)
+    levels(f.long$variable) <- c("F1", "F2", "F3", "F4")
+    
+    ggplot(f.long, aes(x=M2, y=value)) +
+      facet_wrap(~variable, ncol=4) +
+      #geom_point(size=3, color = "#FB9300", alpha=0.3) + # abiotic orange
+      geom_point(size=3, alpha=0.3) +
+      scale_y_continuous(name="Function") +
+      scale_x_continuous(name="Mechanism 2 (M2)") +
+      theme_bw(base_size=15) +
+      #stat_smooth(method="lm", colour="black", size=1, se=F) +
+      theme(panel.grid = element_blank(), strip.background = element_blank())
+    
+  })
+  
+  # plot D: correlation matrix
+  output$plotD <- renderPlot({
+    
+    m <- cor(dt()[,c("F1", "F2", "F3", "F4")])
+    p.mat <- cor.mtest(dt()[,c("F1", "F2", "F3", "F4")])
+    
+    col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+    corrplot(m, method="color", col=col(200),  order="alphabet", number.cex=1.25,
+             type="upper", outline=TRUE,
+             addCoef.col = "black", # Add coefficient of correlation
+             tl.col="black", tl.srt=90, tl.cex = 1.5, #Text label color and rotation
+             # Combine with significance
+             p.mat = p.mat, sig.level = 0.05, insig = "blank", 
+             # hide correlation coefficient on the principal diagonal
+             diag=FALSE 
+    )
+    
+
+  })
+  
+  # plotE: EMF
+  output$plotE <- renderPlot({
+    
+    ggplot(dt(), aes(x=GCD, y=meanFunction)) +
+      geom_point(size=3, alpha=0.2) +
+      theme_bw(base_size=15) +
+      scale_y_continuous(name="Mean function", limits=c(0,1)) +
+      scale_x_continuous(name="Global change driver") +
+      stat_smooth(method="lm", colour="black", size=1, se=T) +
+      theme(panel.grid = element_blank())
+    
+  })
+    
+  
+  output$plotF <- renderPlot({
+    
+    emf.long <- melt(dt(), id.vars="GCD", measure.vars = c("funcMaxed25", "funcMaxed50", "funcMaxed75"))
+    levels(emf.long$variable) <- c("25% threshold", "50% threshold", "75% threshold")
+    
+    ggplot(emf.long, aes(x=GCD, y=value)) +
+      facet_wrap(~variable) +
+      geom_point(size=3, alpha=0.2) +
+      scale_y_continuous(name="Number of functions") +
+      scale_x_continuous(name="Global change driver") +
+      theme_bw(base_size=15) +
+      geom_smooth(method="glm" ,colour="black", size=1, se=T) +  # not an appropriate model
+      theme(panel.grid = element_blank(), strip.background = element_blank())
+    
+  })
+  
+  # plotG: SEM
+  output$plotG <- renderPlot({
+    
+        if(input$dir == "left") { 
+        model.l <-  paste0("
+                  # regressions
+                  M1 ~ GCD + M2
+                  M2 ~ GCD 
+                  ",
+                  input$SEMresponse, " ~ M1 + M2
+                  ")
+        fit <- sem(model.l, data=dt())
+        }
+        if(input$dir == "right") { 
+          model.r <-  paste0("
+                           # regressions
+                           M1 ~ GCD
+                           M2 ~ GCD  + M1
+                           ",
+                           input$SEMresponse, " ~ M2 + M1
+                           ")
+          fit <- sem(model.r, data=dt())
+        }
+        #summary(fit, standardized=TRUE)
+        
+        # plot SEM
+        semPaths(fit, "std", residuals=F, edge.label.cex = 3, sizeMan = 20, sizeMan2=10,
+                 #nodeLabels=c("M1","M2", "EMF", "GCD"), color=c("#049C8E","#FB9300","grey","grey"), # green/orange
+                 nodeLabels=c("M1","M2", "EMF", "GCD"), color=c("grey","grey","grey","grey"),
+                 layout=cbind(c(-1,1,0,0),c(0,0,-1,1)))
+        
+  })
+})
+
+
+
+
+
+
+
+
+
+
---
+++ ui.R
@@ -0,0 +1,225 @@
+
+# Packages
+library(shiny)
+library(simstudy)
+library(corrplot)
+library(reshape2)
+library(devtools)
+library(multifunc)
+library(ggplot2)
+library(gridExtra)
+library(cowplot)
+library(lavaan)
+library(semPlot)
+library(dplyr)
+library(irlba)
+
+# Define UI
+shinyUI(
+  fluidPage(
+
+  # Title
+  titlePanel("Generate hypotheses for global change effects on ecosystem multifunctionality"),
+  
+  fluidRow(
+    column(3, style = "background-color:#F2F2F2;",
+
+      column(12, 
+
+        # input for Step 1
+        br(),
+        h5("Step 1"),
+        radioButtons(inputId = "dir", "Direction of arrow a", choices =c("left", "right"), inline=T)
+      ),
+      
+      column(8, 
+        
+        # input for Step 1
+        # left
+        conditionalPanel( condition = "input.dir == 'left'",
+               textInput(inputId="eqn1.l", label="Eqn 1: mean M1", value="1*GCD + 0*M2"),
+               textInput(inputId="eqn2.l", label="Eqn 2: mean M2", value="1*GCD")
+        ),
+      
+        # right
+        conditionalPanel( condition = "input.dir == 'right'",
+                textInput(inputId="eqn1.r", label="Eqn 1: mean M1", value="1*GCD"),
+                textInput(inputId="eqn2.r", label="Eqn 2: mean M2", value="1*GCD + 0*M1")
+        ),
+        
+        # input for Step 2
+        br(),
+        h5("Step 2"),
+        textInput(inputId="eqn3", label="Eqn 3: mean F1", value="1*M1 + 1*M2"),
+        textInput(inputId="eqn4", label="Eqn 4: mean F2", value="1*M1 + 1*M2"),
+        textInput(inputId="eqn5", label="Eqn 5: mean F3", value="1*M1 + 1*M2"),
+        textInput(inputId="eqn6", label="Eqn 6: mean F4", value="1*M1 + 1*M2")
+       
+      ),
+      
+      column(4,  
+               
+           # Step 1 variances
+           # left
+           conditionalPanel( condition = "input.dir == 'left'",
+                             textInput(inputId = "eqn1var.l", label="variance", value=8, width="75%"),
+                             textInput(inputId = "eqn2var.l", label="variance", value=8, width="75%")
+           ),
+           
+           # right
+           conditionalPanel( condition = "input.dir == 'right'",
+                             textInput(inputId = "eqn1var.r", label="variance", value=8, width="75%"),
+                             textInput(inputId = "eqn2var.r", label="variance", value=8, width="75%")
+           ),
+        
+           # Step 2 variances
+           br(),
+           br(),
+           br(),
+           textInput(inputId = "eqn3var", label="variance", value=8, width="75%"),
+           textInput(inputId = "eqn4var", label="variance", value=8, width="75%"),
+           textInput(inputId = "eqn5var", label="variance", value=8, width="75%"),
+           textInput(inputId = "eqn6var", label="variance", value=8, width="75%")
+           
+      ),
+      column(4,
+        br(),
+        textInput(inputId = "nobs", label="Number of observations", value = 50, width = "100%")
+      ),
+      column(4, 
+        br(),
+        numericInput(inputId = "nlev", label="Number of GCD levels", value = 5)
+      ),
+      column(4,
+        br(),
+        br(),
+        br(),
+        actionButton("gen.data", "Generate data")     
+      ),
+      
+      column(12, 
+             
+        # input for Step 3
+        br(),
+        h5("Step 3"),
+        radioButtons(inputId = "SEMresponse", "Calculation method for SEM", inline=F,
+                     choiceNames =c("average", "25% threshold", "50% threshold", "75% threshold"),
+                     choiceValues =c("meanFunction", "funcMaxed25", "funcMaxed50", "funcMaxed75") ),
+        #actionButton("updateSEM", "Update SEM"),
+        hr()
+        
+      )
+    ),
+    
+    # main panel
+    column(9,
+           
+      tabsetPanel(id="main",
+          tabPanel("About",
+        
+             column(8,
+              
+               # update this in word doc  
+               br(),  
+               
+               h3("Introduction"),
+               h5("This application is designed to assist researchers investigating the effects of global change on ecosystem multifunctionality (i.e. GC-EMF research) to develop mechanistic hypotheses. The application accompanies Giling et al. (in prep). Please see the paper for a comprehensive description of the framework presented here."),
+               h5("The user provides hypothesized effect sizes for 1) the effect of a global change driver on biotic and/or abiotic mechanisms that drive functioning (e.g. species diversity or water availability), and 2) the influence of these mechanisms on multiple individual functions. The application then generates a simple dataset based on these hypotheses to explore the multifunctionality responses using both the average and threshold approaches. In particular, these simulations can be used to quickly visualize and disentangle how the interacting ecological and mathematical effects underlying the conceptual model influence the emergent GC-EMF relationship."),
+               br(),
+               
+               h3("Instructions"),
+               h5("Hypotheses are entered in the sidebar on the left. Each equation represents one or two arrows from the conceptual model below. A zero (e.g. '0*A1' or just '0') indicates no effect. Interactive effects can also be added (e.g. '1*A1*B1'). The arrow ‘a’ can be set as pointing left or right (but not bidirectional). To exclude arrow a, simply do not include M1 or M2 as a predictor variable in Eqn. 1 or 2."),
+               h5("Click 'Generate data' after making hypotheses to run a simulation"),
+               br()
+                 
+             ),
+             
+             column(12,
+                           
+               img(src='workflow_box_fig1b.png', align = "left")       
+                     
+             ),
+             
+             column(8, 
+                    
+               h5("Figure 1. Conceptual framework demonstrating the suggested steps for building mechanistic hypotheses of global change effects on ecosystem multifunctionality. Step 1 entails defining the effect of a global change driver on mechanisms that may be important for ecosystem function (blue arrows). Two mechanisms are displayed here (M1 and M2), and they may influence each other (horizontal blue arrow a). At Step 2, the hypothesized additive or interactive effects of these mechanisms on individual functions (F1-F4) is defined (red arrows). Finally, Step 3 involves considering how the method of multifunctionality calculation affects the interpretation of emergent global change effects (green brace)."),     
+                    
+               br(),
+               h3("Methods"),
+               h5("Data are generated according to the relationships specified by the user using the simstudy package (Goldfeld 2018). The correlation matrix displays positive (blue) and negative (red) correlations that are significant at the level p = 0.05. Multifunctionality metrics are calculated from the simulated data using the multifunc package (Byrnes et al. 2014; Byrnes 2017). Lines of model fit are modelled by stat_smooth in ggplot2 (Wickham 2009) with 'lm' and 'glm' for the averaging and threshold approach, respectively. Predictions are shown for visual aid only and no checks are made on model fit or normality and dispersion assumptions. A structural equation model is fitted using lavaan (Rosseel 2012) and plotted with semPlot (Epskamp 2017) (again there are no checks on fit). All predictions will be invalidated when relationships are non linear."),
+               br(),
+               
+               h3("References"),
+               h5("Byrnes, J (2017). multifunc: Analysis of Ecological Drivers on Ecosystem Multifunctionality. R package version 0.8.0. https://github.com/jebyrnes/multifunc"),
+               h5("Byrnes, J. E. K. et al. Investigating the relationship between biodiversity and ecosystem multifunctionality: challenges and solutions. Methods in Ecology and Evolution 5, 111-124, doi:10.1111/2041-210X.12143 (2014)."),
+               h5("Epskamp, S and with contributions from Simon Stuber (2017). semPlot: Path Diagrams and Visual Analysis of Various SEM Packages' Output. R package version 1.1. https://CRAN.R-project.org/package=semPlot"),
+               h5("Goldfeld, K (2018). simstudy: Simulation of Study Data. R package version 0.1.9. https://CRAN.R-project.org/package=simstudy"),
+               h5("Rosseel, Y (2012). lavaan: An R Package for Structural Equation Modeling. Journal of Statistical Software, 48(2), 1-36. URL http://www.jstatsoft.org/v48/i02/."),
+               h5("Wickham, H. ggplot2: Elegant Graphics for Data Analysis. Springer-Verlag New York, 2009."),
+               
+               br()
+               
+             )  
+          ),
+         
+          tabPanel("Data simulation results",
+            
+             column(12, align="left",
+               br(),
+               tags$b("Step 1: Simulated effect of global change on mechanisms (equations 1 and 2)"),
+               br(),
+               br(),
+               plotOutput("plotA", width = "45%", height = 175), # Eqn 1/2
+               hr()
+             ),
+             
+
+             column(8, align="left",
+               tags$b("Step 2: Simulated effect of mechanisms on individual functions (equations 3-6)", align="left"),
+               plotOutput("plotB", width = "75%", height = 190), # Eqn 3-6
+               plotOutput("plotC", width = "75%", height = 190),
+               hr()
+             ),
+             
+             column(4, align="center",
+               h5("Correlation matrix for functions 1-4"),
+               br(),
+               br(),
+               plotOutput("plotD", width = "100%", height = 300), # correlation matrix
+               br(),               
+               br(),
+               hr()
+             ), 
+              
+             column(9,
+                    tags$b("Step 3: Effect of correlation structure and multifunctionality metric on GC-EMF relationship", align="center")
+             ),
+             column(3,
+                    h5("Structrual Equation Model", align="center")
+             ),
+             
+             column(3, align="center",
+               h5("Averaging approach"),
+               plotOutput("plotE", width = "90%", height = 250) # EMF
+             ), 
+             column(6, align="center",
+               h5("Threshold approach"),
+               plotOutput("plotF", width = "90%", height = 250) # EMF
+             ), 
+             column(3, align="center",
+               plotOutput("plotG", width = "100%", height = 250) # SEM
+             )
+          )
+        )
+      )
+    )
+  )
+)
+
+
+
+
+
+
+
+
